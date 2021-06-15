62a0f7b531637fe24812ecdcbceffeb44785f405
hjpark <hjpark@ksmartech.com>
2021-06-15 14:15:51 +0900

    [추가] 스토어 변경 (애피바이러스) - 1  - 나이스페이 연동 코드 적용  - 테스트 url 적용

--- app/src/main/java/com/genesis/apps/comm/model/constants/StoreInfo.java
+++ app/src/main/java/com/genesis/apps/comm/model/constants/StoreInfo.java
@@ -5,7 +5,10 @@ public interface StoreInfo {
 //    String[] STORE_SEARCH_INFO = {"http://devagenesisproduct.auton.kr/search/gen/ko/list", "https://agenesisproduct.auton.kr/search/gen/ko/list"};
 //    String[] STORE_CART_INFO = {"http://devagenesisproduct.auton.kr/cart/gen/ko/cart_list", "https://agenesisproduct.auton.kr/cart/gen/ko/cart_list"};
 //    String[] STORE_PURCHASE_INFO = {"http://devagenesisproduct.auton.kr/mypage/gen/ko/purchase/list", "https://agenesisproduct.auton.kr/mypage/gen/ko/purchase/list"};
-    String[] STORE_LIST_INFO = {"https://agenesisproduct.auton.kr/ko/main", "https://agenesisproduct.auton.kr/ko/main"};
+
+
+//    String[] STORE_LIST_INFO = {"https://agenesisproduct.auton.kr/ko/main", "https://agenesisproduct.auton.kr/ko/main"};
+    String[] STORE_LIST_INFO = {"https://mygenesis-commerce.appyvirus.com/home/", "https://mygenesis-commerce.appyvirus.com/home/"};
     String[] STORE_SEARCH_INFO = {"https://agenesisproduct.auton.kr/search/gen/ko/list", "https://agenesisproduct.auton.kr/search/gen/ko/list"};
     String[] STORE_CART_INFO = {"https://agenesisproduct.auton.kr/cart/gen/ko/cart_list", "https://agenesisproduct.auton.kr/cart/gen/ko/cart_list"};
     String[] STORE_PURCHASE_INFO = {"https://agenesisproduct.auton.kr/mypage/gen/ko/purchase/list", "https://agenesisproduct.auton.kr/mypage/gen/ko/purchase/list"};
